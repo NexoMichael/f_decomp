dd6a1693b9f2444b2948de57ca13ca1b299464e1
Mikhail Kochegarov <nexo@mkochegarov.rhonda.vtc.ru>
2013-05-13 15:14:10 +1100

    Init

---
+++ f_decomp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  f_decomp
+//
+//  Created by Mikhail Kochegarov on 5/13/13.
+//  Copyright (c) 2013 Mikhail Kochegarov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+}
+
